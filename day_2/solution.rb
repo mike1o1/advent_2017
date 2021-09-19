ddb6d0df685a63cf978db02f0cfd6e834821a623
Mike Wilson <mike1234@gmail.com>
2021-09-18 23:46:37 -0700

    Day 2 solution 2

--- day_2/solution.rb
+++ day_2/solution.rb
@@ -15,7 +15,12 @@ class Day2
   end
 
   def problem_2
+    sum = 0
+    input.each_line do |row|
+      sum += calculate_row_division(row)
+    end
 
+    sum
   end
 
   private
@@ -25,8 +30,34 @@ class Day2
   end
 
   def calculate_row_difference(row)
-    values = row.split(" ").each.map(&:to_i)
+    values = get_row_values(row)
 
     values.max - values.min
+  end
+
+  def calculate_row_division(row)
+    values = get_row_values(row)
+
+    item_1 = 0
+    item_2 = 0
+
+    values.each_with_index do |value, index|
+      values.each_with_index do |second_value, second_index|
+        next if second_index == index
+
+        next if second_value > value
+
+        if value % second_value == 0
+          item_1 = value
+          item_2 = second_value
+        end
+      end
+    end
+
+    item_1 / item_2
+  end
+
+  def get_row_values(row)
+    row.split(" ").each.map(&:to_i)
   end
 end
--- test/day_2_tests.rb
+++ test/day_2_tests.rb
@@ -3,10 +3,32 @@ require_relative "../day_2/solution"
 
 class Day2Tests < Minitest::Test
   def test_day_2_calculates_checksum
-    input = "5 1 9 5\n7 5 3 \n2 4 6 8"
+    input = "5 1 9 5\n7 5 3 \n2 4 6 8\n"
 
     solution = Day2.new(input).problem_1
 
     assert_equal 18, solution
+  end
+
+  def test_day_2_calculates_division_check
+    input = "5 9 2 8\n9 4 7 3\n3 8 6 5\n"
+
+    solution = Day2.new(input).problem_2
+
+    assert_equal 9, solution
+  end
+
+  def test_day_2_solution_1_input
+    input = File.read("day_2/input.txt")
+    answer = Day2.new(input).problem_1
+
+    assert_equal 45351, answer
+  end
+
+  def test_day_2_solution_2_input
+    input = File.read("day_2/input.txt")
+    answer = Day2.new(input).problem_2
+
+    assert_equal 275, answer
   end
 end
