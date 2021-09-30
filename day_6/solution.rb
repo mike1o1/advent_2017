b45024bd087f37e18ef1f11c06dd8e16d11748c1
Mike Wilson <mike1234@gmail.com>
2021-09-29 22:23:23 -0700

    Day 6 solution 2

--- day_6/solution.rb
+++ day_6/solution.rb
@@ -36,7 +36,7 @@ class Day6
       no_duplicates = findings.dig(key).nil?
 
       if no_duplicates
-        findings[key] = 1
+        findings[key] = iteration_count
       end
     end
 
@@ -44,7 +44,41 @@ class Day6
   end
 
   def problem_2
+    length = input.length
 
+    findings = Hash.new
+
+    no_duplicates = true
+    iteration_count = 0
+
+    while no_duplicates do
+      iteration_count += 1
+
+      max = input.max
+      start_position = input.index(max)
+
+      # clear the input
+      input[start_position] = 0
+
+      (1..max).each do |i|
+        next_position = start_position + i
+
+        # we need to loop through the array.
+        next_position = next_position % length
+
+        input[next_position] += 1
+      end
+
+      key = input.join("|")
+
+      no_duplicates = findings.dig(key).nil?
+
+      if no_duplicates
+        findings[key] = iteration_count
+      end
+    end
+
+    iteration_count - findings[input.join("|")]
   end
 
   private
